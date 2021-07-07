63a513aefc9062f4f24127810358718b9a53d46c
TFM110 <tauseef.afraz.1@gmail.com>
2021-07-07 12:27:25 -0500

    recaptcha is back, hopefully

--- assets/js/main.js
+++ assets/js/main.js
@@ -252,5 +252,10 @@
       mirror: false
     })
   });
-
+  window.onload = function() { 
+    var el = document.getElementById('g-recaptcha-response'); 
+    if (el) { 
+      el.setAttribute('required', 'required'); 
+    } 
+  }
 })()
