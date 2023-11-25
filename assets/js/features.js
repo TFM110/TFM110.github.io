9586925ac2b15dcc2b1694090a2fb5aa7653b3c8
Tauseef Afraz <tauseef.afraz.1@gmail.com>
2023-11-25 02:36:38 -0600

    reCAPTCHA V3?

--- assets/js/features.js
+++ assets/js/features.js
@@ -92,40 +92,51 @@ function showSlides(n) {
 /**
  * Contact Form
  */
+const siteKey = '6LcW_xspAAAAAK4bMzJ8aJi6PhMzQvHZ8QqOE5gU';
 const url = 'https://script.google.com/macros/s/AKfycbyAQDr_PwlWDI_6sGqiTNkdc0T4BX8AWeLmCOtf3Iv-f9WlYh4m2XodInEuF42yutvcBQ/exec';
 const contactForm = document.getElementById('contact-form');
 const loadingMessage = document.querySelector('.loading');
 const errorMessage = document.querySelector('.error-message');
 const sentMessage = document.querySelector('.sent-message');
-  
-contactForm.addEventListener('submit', function (event) {
+
+function handleSubmit(event) {
     event.preventDefault();
-  
+
     loadingMessage.style.display = 'block'; // Show loading message
-  
-    const formData = new FormData(this);
-    const data = Object.fromEntries(formData);
-  
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'text/plain;charset=utf-8',
-        },
-        body: JSON.stringify(data),
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        console.log('Successful', data);
-        this.reset();
-        loadingMessage.style.display = 'none'; // Hide loading message
-        sentMessage.style.display = 'block'; // Show success message
-        setTimeout(function () {
-            sentMessage.style.display = 'none'; // Hide success message after a few seconds
-        }, 5000);
-    })
-    .catch((err) => {
-        console.log('err', err);
-        loadingMessage.style.display = 'none'; // Hide loading message
-        errorMessage.style.display = 'block'; // Show error message
+
+    // Get reCAPTCHA token
+    grecaptcha.ready(function () {
+        grecaptcha.execute(siteKey, { action: 'submit' }).then(function (token) {
+            const formData = new FormData(contactForm);
+            const data = Object.fromEntries(formData);
+
+            // Add reCAPTCHA token to form data
+            data.gCaptchaResponse = token;
+
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'text/plain;charset=utf-8',
+                },
+                body: JSON.stringify(data),
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log('Successful', data);
+                    contactForm.reset();
+                    loadingMessage.style.display = 'none'; // Hide loading message
+                    sentMessage.style.display = 'block'; // Show success message
+                    setTimeout(function () {
+                        sentMessage.style.display = 'none'; // Hide success message after a few seconds
+                    }, 5000);
+                })
+                .catch((err) => {
+                    console.log('err', err);
+                    loadingMessage.style.display = 'none'; // Hide loading message
+                    errorMessage.style.display = 'block'; // Show error message
+                });
+        });
     });
-});
+}
+
+document.getElementById('contact-form').addEventListener('submit', handleSubmit);
