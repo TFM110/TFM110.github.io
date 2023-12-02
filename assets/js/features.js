5d791ea47bf13e3673d7700fd4b89837df46a765
Tauseef Afraz <tauseef.afraz.1@gmail.com>
2023-12-02 06:17:35 -0600

    removed gallery

--- assets/js/features.js
+++ assets/js/features.js
@@ -54,42 +54,6 @@ function hideElementsBasedOnDate() {
 hideElementsBasedOnDate();
   
 /**
- * Gallery
- */
-let slideIndex = 1;
-showSlides(slideIndex);
-  
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-  
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-  
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("demo");
-    let captionText = document.getElementById("caption");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}
-  
-/**
  * Contact Form
  */
 const url = 'https://script.google.com/macros/s/AKfycbyAQDr_PwlWDI_6sGqiTNkdc0T4BX8AWeLmCOtf3Iv-f9WlYh4m2XodInEuF42yutvcBQ/exec';
